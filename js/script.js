99ccf934316ee0b691058fc87716be0fde922257
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-07 18:11:21 +0100

    fix: fixed fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 //scrivo in console tutti i numeri da uno a cento, poi divido quelli divisibili per tre con fizz, per 5 con buzz e per entrambi con fizzbuzz, poi li scrivo in console
 for(let i = 1; i <= 100; i++) {
     let numberDIvision;
-    if(i % 3 === 0) {
-        numberDIvision = 'fizz'   
+    if(i % 3 && i % 5 === 0) {
+        numberDIvision = 'fizzbuzz';   
     } else if(i % 5 === 0) {
-        numberDIvision = 'buzz'
+        numberDIvision = 'buzz';
+    } else if(i % 3 === 0) {
+        numberDIvision = 'fizz';
     }
  
     console.log(i + ' - ' + numberDIvision);
-
 }
