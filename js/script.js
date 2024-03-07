1f7665f350b82c730329d037ade9f281a1efbd8a
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-07 18:18:42 +0100

    add comments and html id

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-//scrivo in console tutti i numeri da uno a cento, poi divido quelli divisibili per tre con fizz, per 5 con buzz e per entrambi con fizzbuzz, poi li scrivo in console
+//scrivo in console tutti i numeri da uno a cento, poi... 
 for(let i = 1; i <= 100; i++) {
     let numberDIvision;
+    //quelli idvisibili per tre e cinque scrivo fizzbuzz
     if(i % 3 && i % 5 === 0) {
-        numberDIvision = ' fizzbuzz';   
+        numberDIvision = ' fizzbuzz'; 
+    //quelli per 5 con buzz
     } else if(i % 5 === 0) {
         numberDIvision = ' buzz';
+
+    //quelli divisibili per tre con fizz,
     } else if(i % 3 === 0) {
         numberDIvision = ' fizz';
     } else
     numberDIvision = ''
- 
+
+    //poi li scrivo in console
     console.log(i + numberDIvision);
+
+    //poi li scrivo nel HTML
 }
