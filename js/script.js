b687ccd42e993afac0911361dc45ca026e51dd5f
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-07 17:56:43 +0100

    feat: add fizz and buzz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 //scrivo in console tutti i numeri da uno a cento, poi divido quelli divisibili per tre con fizz, per 5 con buzz e per entrambi con fizzbuzz, poi li scrivo in console
 for(let i = 1; i <= 100; i++) {
-    console.log(i)
+    let numberDIvision;
+    if(i % 3 === 0) {
+        numberDIvision = 'fizz'   
+    } else if(i % 5 === 0) {
+        numberDIvision = 'buzz'
+    }
+ 
+    console.log(i + ' - ' + numberDIvision);
+
 }
