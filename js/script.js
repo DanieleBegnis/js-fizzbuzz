ac9afec755a775e41b7217f9c4f9d92dc8a26147
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-07 17:49:34 +0100

    Feat: stamp number 1 to 100 in console

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//scrivo in console tutti i numeri da uno a cento, poi divido quelli divisibili per tre con fizz, per 5 con buzz e per entrambi con fizzbuzz, poi li scrivo in console
 for(let i = 1; i <= 100; i++) {
-    
+    console.log(i)
 }
